9e62efee2dd37cb7d664eaba7f6ecef02ea44d0c
alexandra_f <avfriesen@mail.ru>
2024-05-12 23:26:09 +0300

    Testing added

---
+++ HW3_testing/HW_ListNode_error_part2/main.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+//подключаем макросы catch2
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+
+#include <cstdint>
+
+struct ListNode
+{
+public:
+    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
+        : value(value), prev(prev), next(next)
+    {
+        if (prev != nullptr) prev->next = this;
+        if (next != nullptr) next->prev = this;
+    }
+
+public:
+    int value;
+    ListNode* prev;
+    ListNode* next;
+};
+
+
+class List
+{
+public:
+    List()
+        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
+        m_tail(new ListNode(0, m_head))
+    {       
+    }
+
+    virtual ~List()
+    {
+        Clear();
+        delete m_head;
+        delete m_tail;
+    }
+
+    bool Empty() { return m_size == 0; }
+
+    unsigned long Size() { return m_size; }
+
+    void PushFront(int value)
+    {
+        new ListNode(value, m_head, m_head->next);
+        ++m_size;
+    }
+
+    void PushBack(int value)
+    {
+        new ListNode(value, m_tail->prev, m_tail);
+        ++m_size;
+    }
+
+    int PopFront()
+    {
+        if (Empty()) throw std::runtime_error("list is empty");
+        auto node = extractPrev(m_head->next->next);
+        int ret = node->value;
+        delete node;
+        return ret;
+    }
+
+    int PopBack()
+    {
+        if (Empty()) throw std::runtime_error("list is empty");
+        auto node = extractPrev(m_tail);
+        int ret = node->value;
+        delete node;
+        return ret;
+    }
+
+    void Clear()
+    {
+        auto current = m_head->next;
+        while (current != m_tail)
+        {
+            current = current->next;
+            delete extractPrev(current);
+        }
+    }
+
+private:
+    ListNode* extractPrev(ListNode* node)
+    {
+        auto target = node->prev;
+        target->prev->next = target->next;
+        target->next->prev = target->prev;
+        --m_size;
+        return target;
+    }
+
+private:
+    ListNode* m_head;
+    ListNode* m_tail;
+    unsigned long m_size;
+};
+
+TEST_CASE("List can be constructed") {
+    List lst1;
+    CHECK(lst1.Empty() == true);
+   
+    //add some lements to list
+    SECTION("Add and delete elements Back") {
+
+     lst1.PushBack(1.0);
+     lst1.PushBack(2.0);
+     lst1.PushBack(3.0);
+     lst1.PushBack(4.0);
+     CHECK(lst1.Size() == 4);
+
+     lst1.PopBack();
+     CHECK(lst1.Size() == 3);
+     
+    };
+
+    SECTION("Add and delete elements Front") {
+
+        lst1.PushFront(5.0);
+        lst1.PushFront(5.0);
+        lst1.PushFront(1.0);
+        CHECK(lst1.Size() == 3);
+
+        lst1.PopFront();
+        CHECK(lst1.Size() == 2);
+
+    };
+
+ 
+  SECTION("Clear list") {
+        lst1.Clear();
+        CHECK(lst1.Empty() == true);
+    };
+
+
+    SECTION("Delete element Back from empty list") {
+        lst1.PopBack();
+        CHECK(lst1.Size() == 0);
+    };
+
+    SECTION("Delete element Front from empty list") {
+        lst1.PopFront();
+        CHECK(lst1.Size() == 0);
+    };
+}
