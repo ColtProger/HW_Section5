3bbf225c1f9f6dd6c81d6abb6f7e78aceffb79ec
alexandra_f <avfriesen@mail.ru>
2024-05-24 22:44:48 +0300

    HW6 STL2 added

---
+++ HW4_shablony/Task1_vector/Task1_vector.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include<vector>
+#include <algorithm>
+
+template <class T>
+
+std::vector<T> delete_dublicats(std::vector<T> vec)
+{
+     std::sort(vec.begin(), vec.end());  // сортировка
+     auto it = std::unique(vec.begin(), vec.end());
+     vec.erase(it, vec.end());
+
+    return vec;
+}
+
+
+
+int main()
+{
+    std::vector<int> vec={ 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::vector<int> vec2 = delete_dublicats(vec);
+
+    
+    std::vector<int>::iterator it = vec2.begin(); 
+    while (it != vec2.end()) 
+    {
+        std::cout << *it << " "; 
+        it++; 
+    }
+}
+
