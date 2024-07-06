daf319314460a8f70856d4e327f4ae384cd8a626
alexandra_f <avfriesen@mail.ru>
2024-07-06 14:12:09 +0300

    Diploma changed

--- Diploma_parser/INI_parser/IniParser.h
+++ Diploma_parser/INI_parser/IniParser.h
@@ -19,31 +19,73 @@ public:
     std::string getString(const std::string& key, const std::string& section) const;  // Метод получения значения-строки
     
     template<typename T>
-    T getValue(const std::string& key, const std::string& section) const; 
+    T getValue(const std::string& key, const std::string& section) const {
+        staticassert(sizeof(T) == -1, "not implemented type for get_value");
+    };
+    
+    
+    
     //специализация
 
     template<>
-    int getValue<int>(const std::string& key, const std::string& section) const; 
+    int getValue<int>(const std::string& key, const std::string& section) const
+    {
+        std::string value = getString(key, section);
+
+        if (value.empty()) {
+            throw std::runtime_error("Invalid line format: " + key + "=");
+        }
+        else {
+
+            try { 
+                const auto str{ std::stoi(value.c_str()) };
+                return str;
+                }
+            catch (...) {
+                throw std::runtime_error("Type of variable is incorrect");
+            }
+           
+        }
+
+       // return {};
+    }
 
     template<>
-    double getValue<double>(const std::string& key, const std::string& section) const;
+    double getValue<double>(const std::string& key, const std::string& section) const
+    {
+        std::string value = getString(key, section);
+
+        if (value.empty()) {
+            throw std::runtime_error("Invalid line format: " + key + "=");
+        }
+        else {
+
+            try {
+                const auto str{ std::stof(value.c_str()) };
+                return str;
+            }
+            catch (...) {
+                throw std::runtime_error("Type of variable is incorrect");
+            }
+
+        }
+    };
 
     template<>
-    std::string getValue<std::string>(const std::string& key, const std::string& section) const;  
+    std::string getValue<std::string>(const std::string& key, const std::string& section) const
+    {
+        std::string value = getString(key, section);
+
+        if (value.empty()) {
+            throw std::runtime_error("Invalid line format: " + key + "=");
+        }
+        else {
+           return value;
+        }
+        //return;
+    };
 
 private:
     std::unordered_map<std::string, std::map<std::string, std::string>> ini_data; // Данные INI-файла
 
-    template<typename T>
-    T convert(const std::string& str, bool& aOk = nullptr) const; // Шаблонный метод для конвертации значений
-
-    //специализация
-    template<>
-    int convert<int>(const std::string& str, bool& aOk) const; 
-
-    template<>
-    double convert<double>(const std::string& str, bool& aOk) const; 
-
-    template<>
-    std::string convert<std::string>(const std::string& str, bool& aOk) const; 
 };
--- Diploma_parser/INI_parser/main.cpp
+++ Diploma_parser/INI_parser/main.cpp
@@ -13,18 +13,22 @@ int main() {
        
         IniParser parser("text.ini");
 
-       // проверка int
-       //  auto value = parser.getValue<int>("var2", "Section2");
-       // std::cout << value << std::endl;
+        //проверка int
+         //auto value = parser.getValue<int>("var1", "Section2");
+        //std::cout << value << std::endl;
        
        
         // проверка float
-        auto value1 = parser.getValue<double>("var1", "Section1");
-        std::cout << value1 << std::endl;
+        //auto value1 = parser.getValue<double>("var1", "Section1");
+        //std::cout << value1 << std::endl;
 
         // проверка на поиск некорректной строки
-        //auto value2 = parser.getValue<int>("Mode", "Section4");
-        //std::cout << value2 << std::endl;
+       // auto value2 = parser.getValue<int>("Mode", "Section4");
+       // std::cout << value2 << std::endl;
+
+        // проверка на поиск строки
+        auto value3 = parser.getValue<std::string>("var2", "Section1");
+        std::cout << value3 << std::endl;
 
     }
     catch (const std::exception& e) {
